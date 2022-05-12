1ef11c9b077305a481f40299823effaef528e5cb
Javi Martinez <jmartinez@ecoes.eco>
2022-05-12 10:58:05 +0200

    Version funcionando los remotes con RPL-classic

---
+++ examples/rpl-udp/project-conf.h
@@ -0,0 +1,17 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+
+#define RF_CONF_MODE    RF_MODE_SUB_1_GHZ
+#define NETSTACK_MAX_ROUTE_ENTRIES 500
+#define COOJA_RADIO_CONF_BUFSIZE 2048
+#define PACKETBUF_CONF_SIZE 2048
+#define RPL_CONF_DIS_INTERVAL 1
+// #define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_DBG
+// #define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_DBG
+// #define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_DBG
+// #define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_DBG
+// #define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_DBG
+// #define LOG_CONF_LEVEL_FRAMER                      LOG_LEVEL_DBG
+
+#endif /* PROJECT_CONF_H_ */
--- examples/rpl-udp/udp-server.c
+++ examples/rpl-udp/udp-server.c
@@ -1,50 +1,30 @@
-/*
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
+
 
 #include "contiki.h"
+#include <stdlib.h>
 #include "net/routing/routing.h"
+#include "random.h"
 #include "net/netstack.h"
 #include "net/ipv6/simple-udp.h"
 
+
 #include "sys/log.h"
 #define LOG_MODULE "App"
-#define LOG_LEVEL LOG_LEVEL_INFO
+#define LOG_LEVEL LOG_LEVEL_DBG
 
-#define WITH_SERVER_REPLY  1
 #define UDP_CLIENT_PORT	8765
 #define UDP_SERVER_PORT	5678
 
+#define SEND_INTERVAL		  (5 * CLOCK_SECOND)
+
 static struct simple_udp_connection udp_conn;
+static struct etimer periodic_timer;
 
 PROCESS(udp_server_process, "UDP server");
-AUTOSTART_PROCESSES(&udp_server_process);
-/*---------------------------------------------------------------------------*/
+PROCESS(send_msg_process, "UDP server");
+AUTOSTART_PROCESSES(&udp_server_process, &send_msg_process);
+
+
 static void
 udp_rx_callback(struct simple_udp_connection *c,
          const uip_ipaddr_t *sender_addr,
@@ -54,26 +34,23 @@ udp_rx_callback(struct simple_udp_connection *c,
          const uint8_t *data,
          uint16_t datalen)
 {
-  // LOG_INFO("Received request '%.*s' from ", datalen, (char *) data);
-  // LOG_INFO_6ADDR(sender_addr);
-  // LOG_INFO_("\n");
+  LOG_INFO("Received response '%.*s' from ", datalen, (char *) data);
+  LOG_INFO_6ADDR(sender_addr);
+  LOG_INFO_("\n");
+    // Estas dos de debajo para DALI
+    // printf("%s", data);
+    // memset((char*)data, 0, datalen );
+
+  // float rssi = get_value(RADIO_PARAM_LAST_RSSI);
+  radio_value_t radio_rssi; 
+  NETSTACK_RADIO.get_value(RADIO_PARAM_LAST_RSSI, &radio_rssi);
+  printf("El RSSI medido es de: %i", radio_rssi);
   printf("\n");
-  if(*data == '1'){
-    printf("on");
-    
-  }
-  if(*data == '0'){
-    printf("off");
-  }
-  printf("\r\n");
-  // printf("Received request '%.*s' from ", datalen, (char *) data);
-#if WITH_SERVER_REPLY
-  /* send back the same string to the client as an echo reply */
-  // LOG_INFO("Sending response.\n");
-  simple_udp_sendto(&udp_conn, data, datalen, sender_addr);
-#endif /* WITH_SERVER_REPLY */
 }
-/*---------------------------------------------------------------------------*/
+
+
+
+
 PROCESS_THREAD(udp_server_process, ev, data)
 {
   PROCESS_BEGIN();
@@ -87,4 +64,31 @@ PROCESS_THREAD(udp_server_process, ev, data)
 
   PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
+
+
+PROCESS_THREAD(send_msg_process, ev, data)
+{
+  
+  static unsigned count;
+  static char str[32];
+  uip_ipaddr_t dest_ipaddr;
+  LOG_INFO("%u", count);
+  PROCESS_BEGIN();
+
+
+
+  while(1) {
+      etimer_set(&periodic_timer, CLOCK_SECOND);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+        uip_ip6addr(&dest_ipaddr,0xfd00,0,0,0,0x207,0x7,0x7,0x7);
+        LOG_INFO("Sending request %u to ", count);
+        LOG_INFO_6ADDR(&dest_ipaddr);
+        LOG_INFO_("\n");
+        snprintf(str, sizeof(str), "hello %d", count);
+        simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr);
+        count++;
+
+  }
+
+  PROCESS_END();
+}
